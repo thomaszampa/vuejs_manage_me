5812ed500422dc32fb03dca155de01d2e3d338b3
thomaszampa <thomas.zampa@gmail.com>
2018-12-05 19:51:48 -0600

    Added goals index to home.vue -- unformatted, but working upon login

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,48 @@
 <template>
   <div class="home">
     <h1>{{ "MANAGE.ME" }}</h1>
+
+    <!-- GOALS INDEX - LINK TO SHOW -->
+    <div class="container">
+      <div class="row mt-3 mb-3">
+        <div v-for="goal in goals" class="col-3 mt-3 mb-3" v-bind:key="goal.id">
+          <div class="card" style="width: 18rem;">
+            <!-- <img class="" v-bind:src="" alt="Card image cap" /> -->
+            <div class="card-body">
+              <h3 class="card-subject">{{ goal.subject }}</h3>
+              <p class="card-body">$ {{ goal.body }}</p>
+              <p class="card-start_date">{{ goal.start_date }}</p>
+              <p class="card-start_date">{{ goal.end_date }}</p>
+              <a v-bind:href="`#/goals/${goal.id}`" class="btn btn-primary">Goals Request</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- ... -->
   </div>
 </template>
 
 <style></style>
 
 <script>
+import axios from "axios";
+
 export default {
   data: function() {
     return {
-      message: "manage.me"
+      message: "manage.me",
+      goals: []
     };
   },
-  created: function() {},
+  created: function() {
+    axios.get("http://localhost:3000/api/goals").then(
+      function(response) {
+        console.log(response.data);
+        this.goals = response.data;
+      }.bind(this)
+    );
+  },
   methods: {},
   computed: {}
 };
