99278d963cfcd060f2d917b19112173cbd057972
thomaszampa <thomas.zampa@gmail.com>
2018-12-05 19:18:08 -0600

    Authentication portals updated, tested, and working

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> | <router-link to="/signup">Signup</router-link> |
-      <router-link to="/login">Login</router-link>
+      <router-link to="/login">Login</router-link> |
+      <router-link to="/logout">Logout</router-link>
     </div>
     <router-view />
   </div>
