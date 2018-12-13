5a96a9be28c11a1ddfbd08ac8b41d8602a07a894
thomaszampa <thomas.zampa@gmail.com>
2018-12-12 20:50:17 -0600

    Notification panel and timeline added to home page

--- src/App.vue
+++ src/App.vue
@@ -212,8 +212,6 @@
                     <ul class="dropdown-menu dropdown-user">
                         <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
                         </li>
-                        <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
-                        </li>
                         <li class="divider"></li>
                         <li><a v-on:click="logout();" href="/#/login"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
                         </li>
