8696f54b479d3ada8702bce9a32f4d8d92390506
thomaszampa <thomas.zampa@gmail.com>
2018-12-12 21:28:40 -0600

    Began v-for loop on collapsuible panels on home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,7 @@
         </div>
         <!-- /.col-lg-12 -->
     </div>
-    <!-- Timeline and Notification Panel -->
+    <!-- Collapsible Panel -->
     <div class="row">
       <div class="col-lg-12">
           <div class="panel panel-default">
@@ -16,40 +16,18 @@
               </div>
               <!-- .panel-heading -->
               <div class="panel-body">
-                  <div class="panel-group" id="accordion">
+                  <div v-for="goal in goals" class="panel-group" v-bind:key="goal.id" id="accordion">
                       <div class="panel panel-default">
                           <div class="panel-heading">
                               <h4 class="panel-title">
-                                  <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">Collapsible Group Item #1</a>
+                                  <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+                                  {{ goal.subject }}
+                                  </a>
                               </h4>
                           </div>
                           <div id="collapseOne" class="panel-collapse collapse in">
                               <div class="panel-body">
-                                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                              </div>
-                          </div>
-                      </div>
-                      <div class="panel panel-default">
-                          <div class="panel-heading">
-                              <h4 class="panel-title">
-                                  <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">Collapsible Group Item #2</a>
-                              </h4>
-                          </div>
-                          <div id="collapseTwo" class="panel-collapse collapse">
-                              <div class="panel-body">
-                                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                              </div>
-                          </div>
-                      </div>
-                      <div class="panel panel-default">
-                          <div class="panel-heading">
-                              <h4 class="panel-title">
-                                  <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">Collapsible Group Item #3</a>
-                              </h4>
-                          </div>
-                          <div id="collapseThree" class="panel-collapse collapse">
-                              <div class="panel-body">
-                                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                  {{ goal.body }}
                               </div>
                           </div>
                       </div>
@@ -59,6 +37,9 @@
           </div>
           <!-- /.panel -->
       </div>
+    </div>
+      <!-- Notification Panel -->
+    <div class="row">
       <div class="col-lg-4">
         <div class="panel panel-default">
             <div class="panel-heading">
@@ -120,8 +101,9 @@
         </div>
         <!-- /.panel -->
       </div>
-      <!-- /.col-lg-4 -->
     </div>
+      <!-- /.col-lg-4 -->
+
     <!-- GOALS INDEX - LINK TO SHOW -->
     <div class="container">
       <div class="row">
